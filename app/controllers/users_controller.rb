3344d2d62270a2ee6c80f9f53147115c5fba79ef
Kyle Patterson <toodlestech@gmail.com>
2011-07-16 22:54:15 -0400

    user and user sessions working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user#  User.find #(params[:id])
   end
   
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
   #  c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
   c.login_field :username
-  c.password_field :password
   end
   validates_uniqueness_of :username
   validates_presence_of :password, :email, :username
